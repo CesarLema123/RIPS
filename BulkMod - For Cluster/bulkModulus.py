35095a0f30319ac774ceb2007a9ae784ecc2dbe7
Jemma Fendley <jfendley@t3420a-1159al.ipamnet.ipam.ucla.edu>
2019-07-16 09:41:51 -0700

    jenny's work

---
+++ BulkMod - For Cluster/bulkModulus.py
@@ -0,0 +1,78 @@
+#This script computes a bulk modulus
+
+
+#Import what is needed
+import os
+import numpy as np
+from utils import *
+import pandas as pd
+
+#---------------------------------------------------------
+
+def getRunNum(loopFileName):
+    """
+    a function that caculates the correct run number for the data wanted
+    """
+    n = int( open(loopFileName).readline().split()[0] ) # number of loops
+    runNum = n + 2 # there are two runs before the loop, the minimisation and trial run.
+    return runNum
+
+#-------------------------------------------------------
+
+def equilPress(runNum,numBins,logFileName):
+    """
+    a function to calculate the equilibrium pressure. Inputted is the run number, which is the run number of the run after the LAMMPS simulation deems that equilibrium pressure has been met. The number of bins and the file name of the log file is also inputted. 
+    """
+    df = readLog(logFileName,runNum) #putting the log file into a data frame
+    pressArray = df["Press"].to_numpy() #extracting the pressure vales
+    length = pressArray.size
+    binSize = length // numBins
+    avePress = np.zeros(numBins)
+    for i in range(numBins):
+        start = i*binSize
+        avePress[i] = np.sum(pressArray[start:(start + binSize)])/binSize #averaging the pressure in each of the bins
+    press = np.average(avePress)
+    return press
+
+#------------------------------------------------------
+
+def calcVolume(runNum,logFileName):
+    """
+    a function to calculate the volume for each simulation.
+    """
+    df = readLog(logFileName,runNum)
+    volArray = df["Volume"].to_numpy()
+    vol = volArray[4] #Volume is constant so can just choose any value!
+    return vol
+    
+#---------------------------------------------------------
+
+def bulkMod(numSims,temp = 2000):
+    """
+    a function to run many LAMMPS simulations at constant temperature and calculate a bulk modulus. inputted is the number of simulations and then temperature
+    """
+    pressure = np.zeros(numSims) #initialising
+    volume = np.zeros(numSims)
+    numDeriv = numSims - 1
+    bM = np.zeros(numDeriv)
+    dpdv = np.zeros(numDeriv) 
+    xScale = 1
+    yScale = 1
+    zScale = 1
+    numBins = 10
+    for i in range(numSims):
+        xScale += 0.001*i #the volume is changed for each simulation, but the relative dimensions of the box remain the same so x,y,z are scaled the same
+        yScale += 0.001*i  
+        zScale += 0.001*i
+        os.system("lmp_daily -in in.bulkCuNi -var TEMP " + str(temp) + " -var xscale " + str(xScale) + " -var yscale " + str(yScale) + " -var zscale " + str(zScale) + " -var simNum " + str(i))
+        runNum = getRunNum("loopNum"+str(i)+".txt")
+        logFileName = "log.bulkMod"+str(i)
+        pressure[i] = equilPress(runNum,numBins,logFileName)
+        print("this is pressure = " + str(pressure[i]))
+        volume[i] = calcVolume(runNum,logFileName)
+    for j in range(numDeriv):
+        dpdv[j] = (pressure[j+1]-pressure[j])/(volume[j+1]-volume[j])
+        bM[j] = -volume[j]*dpdv[j]
+    return bM
+    
+
---
+++ BulkMod - For Cluster/writeData.py
@@ -0,0 +1,228 @@
+#!/usr/bin/python3.6
+from numpy.random import random,normal
+import sys
+
+# random([size]) -> numpy array 1 x size with elements in [0.0,1.0)
+# normal( mu , sigma , size ) ->    np array 1 x size of elements from a gaussian distrobution
+#                                   with mean mu and standard deviation sigma
+
+#use terminal option after calling this file to specify number of atoms along each axis
+if (len(sys.argv) > 1):
+    num = int(sys.argv[1])
+else:
+    print("Invalid number of arguments")
+    exit()
+
+
+FILE = "data.CuNi" + str(num)
+HEADER = "Nickle Copper Simulation"
+
+STYLE = "atomic" 
+LATTICETYPE = "fcc"
+INITTEMP = 0 # kelvin (set via random inital velocities
+
+NUMX = num    # number of atoms in the x direction
+NUMY = num
+NUMZ = num
+
+AX,AY,AZ = 4.6,4.6,4.6  # The lattice constant in the 3 directions
+CONCENTRATIONS = lambda a: 1 if a < 0.4 else 2 # A function which takes in a random value in [0,1) and outputs
+                                                # an atom number
+XLO, XHI = -0.02, (NUMX-1)*AX + 0.02
+YLO, YHI = -0.02, (NUMY-1)*AY + 0.02
+ZLO, ZHI = -0.02, (NUMZ-1)*AZ + 0.02
+
+
+
+
+def getNumAtoms(latticeType = "fcc",numX = 10 ,numY = 10 ,numZ = 10):
+    if latticeType == "fcc":
+        numAtoms = NUMX*NUMY*NUMZ + (NUMX-1)*(NUMY-1)*(NUMZ) + (NUMX-1)*(NUMY)*(NUMZ-1) + (NUMX)*(NUMY-1)*(NUMZ-1)
+    elif latticeType == "bcc":
+        numAtoms = "none" 
+    else:
+        return
+    return numAtoms
+
+
+
+
+
+NUM_ATOMS = getNumAtoms(LATTICETYPE,NUMX,NUMY,NUMZ)
+NUM_BONDS = 0 # Not used
+NUM_ANGLES = 0 # Not used
+NUM_DIHEDRALS = 0 # Not used
+NUM_IMPROPERS = 0 # Not used
+
+
+NUM_ATOM_TYPES = 2 # Specify the number of atoms types
+NUM_BOND_TYPES = 0 # Not used
+NUM_ANGLE_TYPES = 0 # Not used
+NUM_DIHEDRAL_TYPES = 0 # Not used
+NUM_IMPROPER_TYPES = 0 # Not used
+
+
+
+
+
+
+KB = 8.617333262e-5
+SIGMA_VEL =  (30 * INITTEMP * KB)**(2/3)
+
+
+BDRY_LIST = ((XLO,XHI),(YLO,YHI),(ZLO,ZHI))
+BDRY_STR = (("xlo","xhi"),("ylo","yhi"),("zlo","zhi"))
+S = " "
+T = "\t"
+N = "\n"
+
+
+
+
+def writeatom(wfile,style = "atomic",num = 0,mol_tag = 0,atom_tag = 0,q = 0,x=0.0,y=0.0,z=0.0,nx=0.0,ny=0.0,nz=0.0, noise = 0.01):
+    '''wfile is a "_io.TextIOWrapper" for open(FILE),
+    style corresponds to a lammps atom_style which only write the entries which the given atom_style is expecting. 
+    If you spcify a value which is not used by the given style, it will simply be ignored.
+    n is the number of the atom
+    mol_tag is the molecule tag
+    atom_tag is the number of the atom type
+    q is the charge
+    x,y,z are the spacial coordinates
+    nx,ny,nz are optional
+    noise adds a random factor of magnitude noise to each of the positions'''
+    x,y,z = round(x - noise + 2*random(1)[0]*noise,4), round(y - noise + 2*random(1)[0]*noise,4), round(z - noise + 2*random(1)[0]*noise,4)
+    if style == "atomic":
+        s = T + str(num) + T + str(atom_tag) + T + str(x) + T + str(y) + T + str(z) + N
+    elif style == " full":
+        s = T + str(num) + T + str(mol_tag) + T + str(atom_tag) + T + str(q) + T + str(x) + T + str(y) + T + str(z) + T + str(nx) + T + str(ny) + T + str(nz) + N
+    wfile.write(s)
+    return
+
+
+def writevelocity(wfile,style = "atomic",num = 0,vx = 0.0,vy = 0.0, vz = 0.0,noise = 0.01):
+    vx += normal(0,noise,1)[0]
+    vy += normal(0,noise,1)[0]
+    vz += normal(0,noise,1)[0]
+    if not style in ["electron","ellipsoid","sphere","hybrid"]:
+        wfile.write(T + str(num) + T + str(vx) + T + str(vy) + T + str(vz) + N )
+    else:
+        pass
+    return
+
+
+def linebreak(wfile):
+    wfile.write("\n")
+    return
+
+
+# This is the write file
+w = open(FILE,mode = "w")
+
+# Writing the first line of the file
+w.write(HEADER + N)
+
+linebreak(w)
+
+# Writing the number of atoms
+w.write(str(NUM_ATOMS) + S + T + "atoms" + N )
+w.write(str(NUM_BONDS) + S + T + "bonds" + N )
+w.write(str(NUM_ANGLES) + S + T + "angles" + N )
+w.write(str(NUM_DIHEDRALS) + S + T + "dihedrals" + N )
+w.write(str(NUM_IMPROPERS) + S + T + "impropers" + N )
+
+linebreak(w)
+
+# Writing the number of atom types
+w.write(str(NUM_ATOM_TYPES) + T + "atom" + S + "types" + N )
+w.write(str(NUM_BOND_TYPES) + T + "bond" + S + "types" + N )
+w.write(str(NUM_ANGLE_TYPES) + T + "angle" + S + "types" + N )
+w.write(str(NUM_DIHEDRAL_TYPES)  + T + "dihedral" + S + "types" + N )
+w.write(str(NUM_IMPROPER_TYPES)  + T + "improper" + S + "types" + N )
+
+linebreak(w)
+
+#writing x,y,z low and high
+for i in range(len(BDRY_LIST)):
+    if BDRY_LIST[i][0] < 0:
+        w.write(str(BDRY_LIST[i][0]) + T + str(BDRY_LIST[i][1]) + T + BDRY_STR[i][0] + S + BDRY_STR[i][1] + N) 
+    else:
+        w.write(str(BDRY_LIST[i][0]) + T + str(BDRY_LIST[i][1]) +  T + BDRY_STR[i][0] + S + BDRY_STR[i][1] + N) 
+
+linebreak(w)
+
+
+
+
+
+#Writing the atoms sections
+
+w.write("Atoms" + N )
+
+linebreak(w)
+
+n = 1 #atom id will range from 1 to NUM_ATOMS and will also be used to for molecule tag
+charge = 0
+
+
+# Writing the atoms on the verticies of the fcc lattice
+for x in range(NUMX):
+    for y in range(NUMY):
+        for z in range(NUMZ):
+            writeatom(w,style = STYLE,num = n,atom_tag = CONCENTRATIONS(random()), x = AX*x, y = AY*y, z = AZ*z)
+            n+=1
+
+
+
+# Writing the atoms on the 3 faces of the fcc lattice
+
+for x in range(NUMX-1):
+    for y in range(NUMY-1):
+        for z in range(NUMZ):
+            writeatom(w,style = STYLE,num = n,atom_tag = CONCENTRATIONS(random()), x = AX*x + AX/2, y = AY*y + AY/2, z = AZ*z)
+            n+=1
+
+
+for x in range(NUMX-1):
+    for y in range(NUMY):
+        for z in range(NUMZ-1):
+            writeatom(w,style = STYLE,num = n,atom_tag = CONCENTRATIONS(random()), x = AX*x + AX/2, y = AY*y, z = AZ*z + AZ/2)
+            n+=1
+
+
+for x in range(NUMX):
+    for y in range (NUMY-1):
+        for z in range(NUMZ-1):
+            writeatom(w,style = STYLE,num = n,atom_tag = CONCENTRATIONS(random()), x = AX*x, y = AY*y + AY/2, z = AZ*z + AZ/2)
+            n+=1
+
+
+# Writing atom velocities
+
+linebreak(w)
+w.write("Velocities\n")
+linebreak(w)
+
+
+# keeping track of the first N-1 velocities to make sure there is no net velocity
+svx,svy,svz = 0.0,0.0,0.0 
+
+
+for i in range(1,NUM_ATOMS):
+    vx,vy,vz = normal(0,SIGMA_VEL,3)
+    writevelocity(w,style = STYLE,num = i,vx = vx,vy = vy,vz = vz,noise = 0.0)
+    svx += vx
+    svy += vy
+    svz += vz
+
+writevelocity(w,style = STYLE, num = NUM_ATOMS,vx = -svx,vy = -svy,vz = -svz,noise = 0.0)
+
+del svx,svy,svz,vx,vy,vz
+linebreak(w)
+
+w.close()
+
+
+
+
+            
+print("All Done!")
