c17128a8551156acdfccf86ac600162ca7f86adb
psypi314 <52503587+psypi314@users.noreply.github.com>
2019-08-01 20:20:48 -0700

    Update tutorialRun.py
    Add options for each bulk property to be computed in tutorialRun.py

--- tutorial/tutorialRun.py
+++ tutorial/tutorialRun.py
@@ -6,8 +6,22 @@ sim.setSimParams(lib = "/home/IPAMNET/tleadbetter/git-RIPS/RIPS/lib",temperature
 
 sim.runSims()
 sim.cleanOutput()
-y,dy,x,dx = sim.calcHeatCapV() #calcThermoExp() calcBulkModT()
-plt.errorbar(x,y, yerr = dy,xerr = dx,fmt = "o")
+
+#uncomment below to calculate Heat Capacity
+#dEdT,ddEdT,nT,dnT = sim.calcHeatCapV()
+#plt.errorbar(nT, dEdt, xerr = dnT, yerr = ddEdT, fmt = "o")
+
+#uncomment below to calculate Thermal Expansion
+#tE,dtE,nT,dnT = sim.calcThermoExp()
+#plt.errorbar(nT, tE, xerr = dnT, yerr = dtE, fmt = "o")
+
+#uncomment below to calculate Bulk Modulus
+latC = [x in range(3.61,3.66,0.01)]
+sim.setSimParams(lib = "/home/IPAMNET/tleadbetter/git-RIPS/RIPS/lib",temperatures = [900,],concPercents = [30,],pressures = [0,],lengths = [3.63*4],runTimes = [100,],inTemplate = "in.NVT", alloy = "custom",latticeConst = latC,timeStep = 0.0001, simType = "nvt",systemSizes = [4,])
+bM,dbM,nV,dnV = sim.calcBulkModT()
+plt.errorbar(nV, bM, xerr = dnV, yerr = dbM, fmt = "o")
+
+
 plt.show()
 print("All Done")
 
