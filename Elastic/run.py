037e2210e4741ea9e1400eacc46d837b5ebebbdc
Travis Leadbetter <tleadbetter@t3420a-1159jl.ipamnet.ipam.ucla.edu>
2019-08-08 09:40:59 -0700

    change run.py

--- Elastic/run.py
+++ Elastic/run.py
@@ -1,7 +1,11 @@
 # Please include the RIPS/lib/PyScripts in the PYTHONPATH variable before running this script
 
+runOne = True
+
 import sims
-sim = sims.elastic(systemSizes = [14,],temperatures = [300,],inTemplate = "in.elasticTemplate",timeStep = 0.0005)
+sim = sims.elastic(lammps = "lammps.q",systemSizes = [14,],temperatures = [300,],inTemplate = "in.elasticTemplate",timeStep = 0.0005)
 sim.setSimParams(lib = "$HOME/RIPS070819/RIPS/lib",alloy = "custom", runTimes = [10,])
+if runOne:
+	sim.setSimParams(concPercents = [30,])
 sim.runElasticSims()
 
