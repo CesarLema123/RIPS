a5218778b73576d2b62635a88b492c16575b7d8b
Travis Leadbetter <tleadbetter@t3420a-1159jl.ipamnet.ipam.ucla.edu>
2019-08-09 11:07:21 -0700

    GB fix

--- GrainBdry/run.py
+++ GrainBdry/run.py
@@ -8,7 +8,7 @@ runOne = True
 sim = sims.GrainBdry(lammps = "lmp_daily -in", runTimes = [10,],temperatures = [1,] + [x for x in range(100,1601,100)])
 orientations = [[14,0,j,0,1,0,-j,0,14] for j in range(1,3)]
 if runOne:
-    orientations = [[14,0,1],[0,1,0],[-j,0,14]]
+    orientations = [[14,0,1,0,1,0,-j,0,14],]
 sim.setOrientations(orientations)
 sim.runGBSims()
 
