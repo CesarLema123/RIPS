8d6532671199386eeac9022704398765a505593a
jemmacfen <52503823+jemmacfen@users.noreply.github.com>
2019-08-21 23:09:17 +0100

    Update runThermExp.sh

--- Bulk/ThermalExp/runThermExp.sh
+++ Bulk/ThermalExp/runThermExp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Add the location of the python scripts to PYTHONPATH (path/to/RIPS/lib/PyScripts)
 export PYTHONPATH="$HOME/git-RIPS/RIPS/lib/PyScripts:$PYTHONPATH"
 
+# Run the thermal expansion python script
 python3 runThermExp.py
