c5843aca27485bf4bc5dbc257e00acde22cfaea6
psypi314 <52503587+psypi314@users.noreply.github.com>
2019-08-21 14:57:52 -0700

    adding concIntEnergy folder manually

---
+++ concIntEnergy/simulationClass.py
@@ -0,0 +1,13 @@
+import os
+
+class Simulation:
+    def __inti__(self,alloy = 'custom',customLatConst = 1 ,systemSize = 0):
+        self.alloy = alloy
+        self.customLatConst = customLatConst
+        self.systemSize = systemSize
+    
+
+    def setParam1:
+        '<code>'
+
+    def getParam1:
